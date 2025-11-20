a76ba0a9b37dfa3d0f754243d088c1a3668c688b
Kricha Dia-Eddine <tmastmms@gmail.com>
2025-11-20 20:54:40 +0800

    Day 1: trying to figure TS out

---
+++ day_one.py
@@ -0,0 +1,12 @@
+import datetime
+
+print("--- ENGINEERING LOG: DAY 1 ---")
+name = input("Enter your name, Cadet: ")
+
+current_time = datetime.datetime.now()
+
+print(f"\nSystem Initialized.")
+print(f"User: {name}")
+print(f"Login Time: {current_time}")
+print("Goal: Senior Software Engineer")
+print("------------------------------")
