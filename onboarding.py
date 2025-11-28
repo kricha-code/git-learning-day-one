21faa46df0411c4c83bd1c0be464de69ae95d272
Kricha Dia-Eddine <tmastmms@gmail.com>
2025-11-28 21:18:47 +0800

    fix: corrected age logic condition

--- onboarding.py
+++ onboarding.py
@@ -4,5 +4,11 @@ try:
     Age = 2025- int(input("Birth Year"))
 
     print(f"Your name is {Name} and you are {Age} years old.")
+    if Age < 18:
+        print("You are a minor. Please get parent permission.")
+    elif Age < 65:
+        print("Welcome to the team!")
+    else:
+        print("You are eligible for the senior pension plan!")
 except:
     print("Oh no! Please type number (like 1999), not words.")
