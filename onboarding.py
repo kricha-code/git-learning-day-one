bbf925c2eb41f264ed6c1f7d46a38e290cfd523f
Kricha Dia-Eddine <tmastmms@gmail.com>
2025-11-28 17:41:36 +0800

    onboarding.py

--- onboarding.py
+++ onboarding.py
@@ -1,4 +1,8 @@
 Name = input("Name")
-Age = 2025- int(input("Birth Year"))
 
-print(f"Your name is {Name} and you are {Age} years old.")
+try:
+    Age = 2025- int(input("Birth Year"))
+
+    print(f"Your name is {Name} and you are {Age} years old.")
+except:
+    print("Oh no! Please type number (like 1999), not words.")
