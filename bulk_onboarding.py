69d1908eb0fab8a44cc0f7dfad955e52d9d59e2a
Kricha Dia-Eddine <tmastmms@gmail.com>
2025-11-30 21:40:26 +0800

    refactor: implemented list unpacking for readability

--- bulk_onboarding.py
+++ bulk_onboarding.py
@@ -3,5 +3,6 @@ from tools import creat_company_email
 employees = [ "Mike Saul" , "Jamie MCguile" , "Walter Black"]
 for individual_name in employees:
     print(individual_name)
-    names = individual_name.split(" ")
-    print("company email: " + creat_company_email(names[0], names[1]))
+    first_name, last_name = individual_name.split(" ")
+    print("company email: " + creat_company_email(first_name, last_name))
+    print("-"*40)
