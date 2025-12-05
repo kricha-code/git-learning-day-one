afb629e00d49e8a1d9f756fffdf5941231547347
Kricha Dia-Eddine <tmastmms@gmail.com>
2025-12-06 03:49:01 +0800

    feat: generated CSV export from text input

--- file_manager.py
+++ file_manager.py
@@ -1,12 +1,28 @@
+from tools import creat_company_email
+
+# 1. READ the data from a text file
 with open("employees.txt", "r") as file:
-    # 2. Read lines into a LIST
     employees = file.readlines()
     
-# 3. Prove it is a list    
-print(employees)
+# 2. WRITE the processed data to a new CSV file
+with open("employees_emails.csv", "w") as file:
 
-# 4. Loop through it (The Conveyor Belt)
-for name in employees:
-    # 5. Clean the name (Remove the invisible "Enter" character \n)
-    clean_name = name.strip()
+    # A. Write the Column Headers for Excel
+    file.write("Name,Email\n")
+
+    # B. The Factory Loop
+    for name in employees:
+
+        # Clean the name (remove the invisible \n from the text file)
+        clean_name = name.strip()
+
+        # Unpack the first and last name
+        first,last = clean_name.split(" ")
+
+        # Generate Email using your tool
+        email = creat_company_email(first, last)
+
+        # Write to file: "Name,Email" and add a New Line (\n) at the end
+        file.write(f"{clean_name},{email}\n")
+
     print(f"Processing : {clean_name}")
